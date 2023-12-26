a24c24304120d9a86a7f7795b51e8d6d431f4519
RuteSantos <1160663@isep.ipp.pt>
2023-12-26 19:56:29 +0000

    style(frontend): category radio buttons

--- expense-tracking-ui/tailwind.config.js
+++ expense-tracking-ui/tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   content: ['*.html'],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        backgroundColor: ['label-checked'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant, e }) => {
+      addVariant('label-checked', ({ modifySelectors, separator }) => {
+        modifySelectors(
+          ({ className }) => {
+            const eClassName = e(`label-checked${separator}${className}`);
+            console.log(eClassName)
+            const yourSelector = 'input[type="radio"]';
+            console.log(yourSelector)
+            return `${yourSelector}:checked ~ .${eClassName}`;
+          }
+        )
+      })
+    }),
+  ],
 }
 
