8b5c2ed57d0740e7d1248e640003cd8082562754
RuteSantos <1160663@isep.ipp.pt>
2024-01-03 22:45:16 +0000

    feat(frontend): Server UI read env config and set it in expense-tracking-ui

--- expense-tracking-ui-server/index.js
+++ expense-tracking-ui-server/index.js
@@ -1,4 +1,12 @@
-const express = require('express')
-const app = express()
-app.use(express.static('../expense-tracking-ui'))
-app.listen(4000)
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const vapidPublicKey = process.env.VAPID_PUBLIC_KEY;
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.get('/', function (req, res) {
+	res.render('../../expense-tracking-ui', { VAPID_PUBLIC_KEY: vapidPublicKey });
+});
+app.use(express.static('../expense-tracking-ui'));
+app.listen(4000);
