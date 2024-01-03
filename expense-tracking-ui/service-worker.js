46eb3ce43c14eeb51c86ac12544d0b0fe883ae0f
RuteSantos <1160663@isep.ipp.pt>
2024-01-03 22:46:18 +0000

    feat(frontend): service worker

---
+++ expense-tracking-ui/service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('push', (event) => {
+    const options = {
+      body: event.data.text(),
+      icon: '/icon.png',
+      badge: '/badge.png',
+    };
+    event.waitUntil(self.registration.showNotification('New Expense', options));
+});
