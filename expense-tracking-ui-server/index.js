dfeb08f969635c8776845ffdd2167bb2f807a155
RuteSantos <1160663@isep.ipp.pt>
2024-01-08 21:15:48 +0000

    feat(frontend): read categories from spreadsheet

--- expense-tracking-ui-server/index.js
+++ expense-tracking-ui-server/index.js
@@ -5,8 +5,17 @@ const app = express();
 const vapidPublicKey = process.env.VAPID_PUBLIC_KEY;
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.get('/', function (req, res) {
-	res.render('../../expense-tracking-ui', { VAPID_PUBLIC_KEY: vapidPublicKey });
+app.get('/', async function (req, res) {
+	res.render('../../expense-tracking-ui', { VAPID_PUBLIC_KEY: vapidPublicKey, CATEGORIES: await getCategories() });
 });
 app.use(express.static('../expense-tracking-ui'));
 app.listen(4000);
+
+        
+async function getCategories() {
+	const response = await fetch('http://127.0.0.1:8080/categories/', {
+		method: 'get',
+	});
+
+	return await response.json();
+}
