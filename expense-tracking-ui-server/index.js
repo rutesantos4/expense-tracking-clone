8535b6cc43cfe098acc1d1cd93574da91baade18
RuteSantos <1160663@isep.ipp.pt>
2024-01-14 19:47:22 +0000

    chore(frontend): configure https

--- expense-tracking-ui-server/index.js
+++ expense-tracking-ui-server/index.js
@@ -1,15 +1,33 @@
 require('dotenv').config();
 const express = require('express');
+const https = require("https");
+var http = require('http');
+const fs = require('fs');
 const app = express();
 
-const vapidPublicKey = process.env.VAPID_PUBLIC_KEY;
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.get('/', async function (req, res) {
-	res.render('../../expense-tracking-ui', { VAPID_PUBLIC_KEY: vapidPublicKey, CATEGORIES: await getCategories() });
+	res.render('../../expense-tracking-ui', { VAPID_PUBLIC_KEY: process.env.VAPID_PUBLIC_KEY, CATEGORIES: await getCategories() });
 });
 app.use(express.static('../expense-tracking-ui'));
-app.listen(4000);
+
+// Read SSL certificate and key files
+const sslOptions = {
+	key: fs.readFileSync(process.env.KEY_FILE_PATH),
+	cert: fs.readFileSync(process.env.CERTIFICATE_FILE_PATH)
+};
+var serverHTTPS = https.createServer(sslOptions, app);
+const httpsPort = process.env.HTTPS_PORT || 4000;
+serverHTTPS.listen(httpsPort, () => {
+	console.log(`App listening on https://localhost:${httpsPort}`);
+});
+
+var serverHTTP = http.createServer(app);
+const httpPort = process.env.HTTP_PORT || 4001;
+serverHTTP.listen(httpPort, () => {
+	console.log(`App listening on http://localhost:${httpPort}`);
+});
 
 
 async function getCategories() {
